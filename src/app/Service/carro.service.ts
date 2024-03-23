10fefed787225d4efd9eaec0db1c933a5c86a49e
joseSalaz <josealvertosalazarchirinos@gmail.com>
2024-03-22 20:03:47 -0500

    Carro completo

--- src/app/Service/carro.service.ts
+++ src/app/Service/carro.service.ts
@@ -1,38 +1,43 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { Libro } from '../Interface/libro';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarroService {
-  private libro: Libro [] = [];
-  
-  private _libro:BehaviorSubject<Libro[]>;
+  private _libro: BehaviorSubject<Libro[]>;
+  private storageKey = 'carroLibros';
 
-  constructor(){
-    this._libro = new BehaviorSubject<Libro[]>
-    ([])
+  constructor() {
+    const storedLibros = JSON.parse(localStorage.getItem(this.storageKey) || '[]');
+    this._libro = new BehaviorSubject<Libro[]>(storedLibros);
   }
 
   obtenerCantidadProductos(): number {
-    return this.libro.length;
+    return this._libro.value.length;
   }
 
-  get Libro (){
+  get Libro() {
     return this._libro.asObservable();
   }
 
-  addNewProduct(libro : Libro) {
-    this.libro.push(libro);
-    this._libro.next(this.libro);
+  addNewProduct(libro: Libro) {
+    const libros = [...this._libro.value, libro];
+    this.updateStorage(libros);
   }
 
-  deleteLibro(index: number){
-    this.libro.splice(index,1)
-    this._libro.next(this.libro);
+  deleteLibro(index: number) {
+    const libros = this._libro.value.filter((_, i) => i !== index);
+    this.updateStorage(libros);
   }
 
-  
+  private updateStorage(libros: Libro[]) {
+    this._libro.next(libros);
+    localStorage.setItem(this.storageKey, JSON.stringify(libros));
+    console.log(libros);
+    
+  }
 }
+
 
