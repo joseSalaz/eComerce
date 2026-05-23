71ef35a9f35e88bc2ea325ee7c37f23c41954760
joseSalaz <josealvertosalazarchirinos@gmail.com>
2026-05-22 19:01:00 -0500

    acttualizado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    endPoint: 'https://localhost:7143/',
+    endPoint: 'https://api.libroapi.store/',
     //endPoint: 'http://localhost:5229/',
     //https://api20250205164724.azurewebsites.net/
 
